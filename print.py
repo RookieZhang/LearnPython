32b189c2206fc326144eb74b6208b30d3eb9280f
RookieZhang <zhanghang@zhanghangdeMacBook-Pro.local>
2015-06-01 00:17:52 +0800

    using plus in print
    1.using + between two string there is not space int output
    2.using + between a string and a variable there is still no any space

--- print.py
+++ print.py
@@ -1,1 +1,4 @@
 print "Hello, welcome to Python world"
+print "hello" + "Python"   #without any space between the two string
+str1 = "Python"
+print "hello" + str1    #even if with a variable there is still no space
