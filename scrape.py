6bb0763ee99a98cca97f02c5cc365c584bdceac4
RookieZhang <262970203@qq.com>
2015-06-07 22:04:35 +0800

    scrapy.py
    first spider program

---
+++ scrape.py
@@ -0,0 +1,28 @@
+import urlparse
+import urllib
+from bs4 import BeautifulSoup
+
+url = "http://tieba.baidu.com"
+
+urls = [url] #stack of urls to scrape
+visited = [url] #history record of urls
+
+while len(urls) > 0:
+	try:
+		htmltext = urllib.urlopen(urls[0]).read()
+	except:
+		print urls[0]
+	soup = BeautifulSoup(htmltext)
+
+	urls.pop(0)
+	print len(urls)
+
+	for tag in soup.findAll('a', href=True):
+		tag['href'] = urlparse.urljoin(url,tag['href'])
+		if url in tag['href'] and tag['href'] not in visited:
+			urls.append(tag['href'])
+			visited.append(tag['href'])
+
+print visited
+
+	
